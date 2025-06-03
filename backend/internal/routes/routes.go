2ed715565fa843978a69cc2ebe1ad0f7a1897f41
JuDyas <gden886@gmail.com>
2025-06-03 19:45:44 +0200

    Add routes package with SetupRoutes function
    Introduced a `SetupRoutes` function to define API endpoints, including root redirection and user authentication routes (`/register` and `/login`).

---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/JuDyas/buy-sell-platform/backend/config"
+	"github.com/JuDyas/buy-sell-platform/backend/internal/app"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func SetupRoutes(e *echo.Echo, envs config.Config, handlers app.Handlers) {
+	var (
+		v1    = e.Group("/api/v1")
+		users = v1.Group("/users")
+	)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "/api/v1/docs")
+	})
+	users.POST("/register", handlers.UserHandler.Register(envs.JWTSecret))
+	users.POST("/login", handlers.UserHandler.Login(envs.JWTSecret))
+}
