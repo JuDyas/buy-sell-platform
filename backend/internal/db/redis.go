ccb0be2f3bda19557c132f4e05032d298fbb38a3
JuDyas <gden886@gmail.com>
2025-06-03 19:19:07 +0200

    Add Redis connection helper with NewRedis function
    Introduced a `NewRedis` function to create and configure a Redis client using the provided URI, enabling easy integration of Redis into the application.

--- backend/internal/db/redis.go
+++ backend/internal/db/redis.go
@@ -1,1 +1,11 @@
 package db
+
+import "github.com/redis/go-redis/v9"
+
+func NewRedis(redisURI string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: redisURI,
+	})
+
+	return client
+}
