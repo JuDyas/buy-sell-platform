13aeb621273eaadbed8191e19e86eaf1a832b436
JuDyas <gden886@gmail.com>
2025-06-04 00:12:16 +0200

    Integrate `AuthMiddleware` into routes for JWT authentication

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/JuDyas/buy-sell-platform/backend/config"
 	"github.com/JuDyas/buy-sell-platform/backend/internal/app"
+	"github.com/JuDyas/buy-sell-platform/backend/internal/middleware"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -13,6 +14,7 @@ func SetupRoutes(e *echo.Echo, envs config.Config, handlers app.Handlers) {
 		users = v1.Group("/users")
 	)
 
+	e.Use(middleware.AuthMiddleware(envs.JWTSecret))
 	e.GET("/", func(c echo.Context) error {
 		return c.Redirect(http.StatusMovedPermanently, "/api/v1/docs")
 	})
