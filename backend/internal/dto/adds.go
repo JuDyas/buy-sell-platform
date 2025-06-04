a024bd755ac41e6a2e840669dd3590139e958115
JuDyas <gden886@gmail.com>
2025-06-04 16:36:04 +0200

    Add DTOs for Advert creation and update operations

--- backend/internal/dto/adds.go
+++ backend/internal/dto/adds.go
@@ -1,1 +1,15 @@
 package dto
+
+type AdvertCreate struct {
+	Title       string   `json:"title" validate:"required"`
+	Description string   `json:"description" validate:"required"`
+	Price       int      `json:"price" validate:"required"`
+	Images      []string `json:"images"`
+}
+
+type AdvertUpdate struct {
+	Title       string   `json:"title,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Price       int      `json:"price,omitempty"`
+	Images      []string `json:"images,omitempty"`
+}
