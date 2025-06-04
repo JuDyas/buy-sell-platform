62e1cb92818dbad9f00de330b78cd04be7579093
JuDyas <gden886@gmail.com>
2025-06-04 16:36:23 +0200

    Add AdvertService with CRUD operations
    Implemented AdvertService interface and its methods including Create, Update, GetByID, and SoftDelete. Integrated repository layer for database interactions to manage adverts.

--- backend/internal/service/adds.go
+++ backend/internal/service/adds.go
@@ -1,1 +1,76 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/JuDyas/buy-sell-platform/backend/internal/dto"
+	"github.com/JuDyas/buy-sell-platform/backend/internal/models"
+	"github.com/JuDyas/buy-sell-platform/backend/internal/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type AdvertService interface {
+	Create(ctx context.Context, authorID primitive.ObjectID, req dto.AdvertCreate) error
+	Update(ctx context.Context, advertID primitive.ObjectID, req dto.AdvertUpdate) error
+	GetByID(ctx context.Context, advertID primitive.ObjectID) (*models.Advert, error)
+	SoftDelete(ctx context.Context, advertID primitive.ObjectID) error
+}
+
+type advertService struct {
+	repo repository.AdvertRepository
+}
+
+func NewAdvertService(repo repository.AdvertRepository) AdvertService {
+	return &advertService{
+		repo: repo,
+	}
+}
+
+func (s *advertService) Create(ctx context.Context, authorID primitive.ObjectID, req dto.AdvertCreate) error {
+	advert := &models.Advert{
+		Title:       req.Title,
+		Description: req.Description,
+		Price:       req.Price,
+		Images:      req.Images,
+		AuthorID:    authorID,
+	}
+
+	err := s.repo.Create(ctx, advert)
+	if err != nil {
+		return fmt.Errorf("failed to create advert: %w", err)
+	}
+
+	return nil
+}
+
+func (s *advertService) Update(ctx context.Context, advertID primitive.ObjectID, req dto.AdvertUpdate) error {
+	update, err := structToBsonMap(req)
+	if err != nil {
+		return fmt.Errorf("failed to convert struct to bson map: %w", err)
+	}
+
+	err = s.repo.Update(ctx, advertID, update)
+	if err != nil {
+		return fmt.Errorf("failed to update advert: %w", err)
+	}
+
+	return nil
+}
+
+func (s *advertService) GetByID(ctx context.Context, advertID primitive.ObjectID) (*models.Advert, error) {
+	advert, err := s.repo.FindByID(ctx, advertID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get advert: %w", err)
+	}
+
+	return advert, nil
+}
+
+func (s *advertService) SoftDelete(ctx context.Context, advertID primitive.ObjectID) error {
+	err := s.repo.SoftDelete(ctx, advertID)
+	if err != nil {
+		return fmt.Errorf("failed to delete advert: %w", err)
+	}
+
+	return nil
+}
