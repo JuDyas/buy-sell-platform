9c4eb3b834931e4ab501fd785fa2209942202088
JuDyas <gden886@gmail.com>
2025-06-04 23:26:26 +0200

    Add UserPublic DTO for public-facing user data

--- backend/internal/dto/user.go
+++ backend/internal/dto/user.go
@@ -1,5 +1,11 @@
 package dto
 
+import (
+	"github.com/JuDyas/buy-sell-platform/backend/internal/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
 type UserRegister struct {
 	Username string `json:"username" validate:"required,min=3,max=32"`
 	Email    string `json:"email" validate:"required,email"`
@@ -18,4 +24,15 @@ type UserUpdate struct {
 	Phone     string `json:"phone,omitempty"`
 	Location  string `json:"location,omitempty"`
 	AvatarURL string `json:"avatar_url,omitempty"`
+}
+
+type UserPublic struct {
+	ID        primitive.ObjectID `json:"id"`
+	Username  string             `json:"username"`
+	Email     string             `json:"email"`
+	AvatarURL string             `json:"avatar_url"`
+	Phone     string             `json:"phone"`
+	Location  string             `json:"location"`
+	Role      models.UserRole    `json:"role"`
+	CreatedAt time.Time          `json:"created_at"`
 }
