0f4e6b644f6c81288b9f051db6a43bef29ecc7a5
JuDyas <gden886@gmail.com>
2025-06-05 09:02:08 +0200

    Add `Category` model and `Update` method to `CategoryRepository` for category management

--- backend/internal/repository/categories.go
+++ backend/internal/repository/categories.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/JuDyas/buy-sell-platform/backend/internal/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"time"
 )
@@ -27,6 +29,19 @@ func (r *categoryRepository) Create(ctx context.Context, category *models.Catego
 	category.CreatedAt = time.Now()
 	category.UpdatedAt = category.CreatedAt
 	_, err := r.coll.InsertOne(ctx, category)
+	if err != nil {
+		return fmt.Errorf("failed to create category: %w", err)
+	}
 
-	return err
+	return nil
+}
+
+func (r *categoryRepository) Update(ctx context.Context, id primitive.ObjectID, update bson.M) error {
+	update["updated_at"] = time.Now()
+	_, err := r.coll.UpdateOne(ctx, bson.M{"_id": id, "is_deleted": false}, bson.M{"$set": update})
+	if err != nil {
+		return fmt.Errorf("failed to update category: %w", err)
+	}
+
+	return nil
 }
