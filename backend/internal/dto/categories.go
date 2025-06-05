8c4a74aa92249aad79ea4a0641877e5cfe905e87
JuDyas <gden886@gmail.com>
2025-06-05 10:15:54 +0200

    Add `CategoryUpdate` DTO and `Update` method to `CategoriesService` for category update operations

--- backend/internal/dto/categories.go
+++ backend/internal/dto/categories.go
@@ -4,3 +4,8 @@ type CategoryCreate struct {
 	Name        string `json:"name" validate:"required"`
 	Description string `json:"description" validate:"required"`
 }
+
+type CategoryUpdate struct {
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+}
--- backend/internal/service/categories.go
+++ backend/internal/service/categories.go
@@ -7,10 +7,12 @@ import (
 	"github.com/JuDyas/buy-sell-platform/backend/internal/models"
 	"github.com/JuDyas/buy-sell-platform/backend/internal/repository"
 	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type CategoriesService interface {
 	Create(ctx context.Context, req dto.CategoryCreate) error
+	Update(ctx context.Context, id primitive.ObjectID, req dto.CategoryUpdate) error
 }
 
 type categoriesService struct {
@@ -32,6 +34,22 @@ func (s *categoriesService) Create(ctx context.Context, req dto.CategoryCreate) 
 	err := s.repo.Create(ctx, &category)
 	if err != nil {
 		log.Error(fmt.Sprintf("Error creating category: %v", err))
+		return err
+	}
+
+	return nil
+}
+
+func (s *categoriesService) Update(ctx context.Context, id primitive.ObjectID, req dto.CategoryUpdate) error {
+	update, err := structToBsonMap(req)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error converting struct to bson map: %v", err))
+		return err
+	}
+
+	err = s.repo.Update(ctx, id, update)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error updating category: %v", err))
 		return err
 	}
 
