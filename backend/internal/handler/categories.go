566a40c0f61f8f207392f4899fc81b83c871f741
JuDyas <gden886@gmail.com>
2025-06-05 10:30:05 +0200

    Add `Update` method to `CategoryHandler` for updating categories

--- backend/internal/handler/categories.go
+++ backend/internal/handler/categories.go
@@ -4,6 +4,7 @@ import (
 	"github.com/JuDyas/buy-sell-platform/backend/internal/dto"
 	"github.com/JuDyas/buy-sell-platform/backend/internal/service"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 )
 
@@ -11,7 +12,7 @@ type CategoryHandler struct {
 	service service.CategoriesService
 }
 
-func NewCategoryHandler(service *service.CategoriesService) *CategoryHandler {
+func NewCategoryHandler(service service.CategoriesService) *CategoryHandler {
 	return &CategoryHandler{
 		service: service,
 	}
@@ -30,5 +31,29 @@ func (h *CategoryHandler) Create() echo.HandlerFunc {
 		}
 
 		return c.JSON(http.StatusCreated, map[string]interface{}{"id": id})
+	}
+}
+
+func (h *CategoryHandler) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var (
+			req dto.CategoryUpdate
+			id  = c.Param("id")
+		)
+
+		if err := c.Bind(&req); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request body"})
+		}
+
+		idObj, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid id"})
+		}
+
+		if err := h.service.Update(c.Request().Context(), idObj, req); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "cannot update category"})
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{"message": "category updated"})
 	}
 }
