56c5b94a44e773972a2f8fd400a841054ffb4135
JuDyas <gden886@gmail.com>
2025-06-15 13:58:29 +0200

    Create `Page` component; integrate `AdvertsGrid` for displaying advertisements within a styled container.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,9 @@
-export default function Home() {
-  return null
+import AdvertsGrid from '@/components/Advert/AdvertGrid';
+
+export default function Page() {
+  return (
+      <div className="min-h-[85vh]">
+        <AdvertsGrid />
+      </div>
+  );
 }
