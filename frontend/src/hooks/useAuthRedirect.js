1ba76a73ca76d46a2eb281216c153e03b211a732
JuDyas <gden886@gmail.com>
2025-06-15 13:25:46 +0200

    Update import path for `useUser` in `useAuthRedirect` to use absolute path

--- frontend/src/hooks/useAuthRedirect.js
+++ frontend/src/hooks/useAuthRedirect.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useUser } from '../context/UserContext';
+import { useUser } from '@/context/UserContext';
 
 export function useAuthRedirect({ redirectTo = "/", fetchOnMount = true } = {}) {
     const router = useRouter();
