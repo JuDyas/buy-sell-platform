ee37b504c0ace1bc8a109e088ac37f3fef179171
JuDyas <gden886@gmail.com>
2025-06-15 13:16:32 +0200

    Add `useAuthRedirect` hook for handling user authentication redirection

---
+++ frontend/src/hooks/useAuthRedirect.js
@@ -0,0 +1,24 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useUser } from '../context/UserContext';
+
+export function useAuthRedirect({ redirectTo = "/", fetchOnMount = true } = {}) {
+    const router = useRouter();
+    const { user, fetchUser } = useUser();
+
+    useEffect(() => {
+        if (fetchOnMount) fetchUser();
+    }, [fetchUser, fetchOnMount]);
+
+    useEffect(() => {
+        if (user) {
+            router.replace(redirectTo);
+        }
+    }, [user, router, redirectTo]);
+
+    return {
+        user,
+        loading: user === undefined,
+    };
+}
