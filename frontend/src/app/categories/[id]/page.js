30e79aae538d902a57838e4730558a1769575aab
JuDyas <gden886@gmail.com>
2025-06-17 08:00:54 +0200

    Add `SearchBar` component to `CategoryPage` and adjust layout height for responsiveness

--- frontend/src/app/categories/[id]/page.js
+++ frontend/src/app/categories/[id]/page.js
@@ -1,9 +1,11 @@
 import AdvertsGrid from "@/components/Advert/AdvertGrid";
+import SearchBar from "@/components/Advert/SearchBar";
 
 export default function CategoryPage({ params }) {
     const { id } = params;
     return (
-        <div className="min-h-[85vh]">
+        <div className="min-h-[83vh] 2xl:min-h-[87vh]">
+            <SearchBar />
             <AdvertsGrid categoryId={id} />
         </div>
     );
